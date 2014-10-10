3a6d0552feb2cb9dd58cb9db457c604e66f653cb
goelvivek2013 <goelvivek2013@gmail.com>
2014-10-10 16:31:41 +0530

    controller name change

--- app/controllers/custom_users_controller.rb
+++ app/controllers/custom_users_controller.rb
@@ -1,4 +1,4 @@
-class UserrsController < ApplicationController
+class CustomUsersController < ApplicationController
 	before_filter :authenticate_user! 
 
   def index
@@ -27,7 +27,7 @@ class UserrsController < ApplicationController
 
 		respond_to do |format|
 			if @user.save
-				format.html { redirect_to :controller=>'userrs',:action=>'index', notice: 'User was successfully created.' }
+				format.html { redirect_to :controller=>'custom_users',:action=>'index', notice: 'User was successfully created.' }
 			else
 			  format.html { render :new , notice: 'error creating user created.' }
 			end
@@ -37,13 +37,13 @@ class UserrsController < ApplicationController
 
   def activate_account
     User.restore(params[:id])
-    redirect_to :controller=>'userrs',:action=>'index'
+    redirect_to :controller=>'custom_users',:action=>'index'
   end
 
   def delete
     user = User.find(params[:id])
     user.delete
-    redirect_to :controller=>'userrs',:action=>'index'
+    redirect_to :controller=>'custom_users',:action=>'index'
   end
 
 end
